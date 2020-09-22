0a1f6001f4b57f81dd8d2489652b0ef3bf79ca37
Agus Mahasadhu <agus.mahasadhu@gmail.com>
2020-09-22 13:36:35 +0800

    add styling for digital artisans branding

--- src/config/backpack/crud.php
+++ src/config/backpack/crud.php
@@ -23,7 +23,7 @@ return [
 
             // enable the datatables-responsive plugin, which hides columns if they don't fit?
             // if not, a horizontal scrollbar will be shown instead
-            'responsiveTable' => true,
+            'responsiveTable' => false,
 
             // stores pagination and filters in localStorage for two hours
             // whenever the user tries to see that page, backpack loads the previous pagination and filtration
@@ -47,7 +47,7 @@ return [
             // A 1D array of options which will be used for both the displayed option and the value, or
             // A 2D array in which the first array is used to define the value options and the second array the displayed options
             // If a 2D array is used, strings in the right hand array will be automatically run through trans()
-            'pageLengthMenu' => [[10, 25, 50, 100, -1], [10, 25, 50, 100, 'backpack::crud.all']],
+            'pageLengthMenu' => [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, 'backpack::crud.all']],
 
             // How important is it for the action buttons to be visible?
             // - 0 - most important
@@ -70,7 +70,7 @@ return [
         'create' => [
             // Define the size/looks of the content div for all CRUDs
             // To override per view use $this->crud->setCreateContentClass('class-string')
-            'contentClass' => 'col-md-8 bold-labels',
+            'contentClass' => 'col-md-12 bold-labels',
 
             // When using tabbed forms (create & update), what kind of tabs would you like?
             'tabsType' => 'horizontal', //options: horizontal, vertical
@@ -106,7 +106,7 @@ return [
         'update' => [
             // Define the size/looks of the content div for all CRUDs
             // To override per view use $this->crud->setEditContentClass('class-string')
-            'contentClass'   => 'col-md-8 bold-labels',
+            'contentClass'   => 'col-md-12 bold-labels',
 
             // When using tabbed forms (create & update), what kind of tabs would you like?
             'tabsType' => 'horizontal', //options: horizontal, vertical
@@ -142,7 +142,7 @@ return [
         'show' => [
             // Define the size/looks of the content div for all CRUDs
             // To override per Controller use $this->crud->setShowContentClass('class-string')
-            'contentClass' => 'col-md-8',
+            'contentClass' => 'col-md-12',
         ],
 
         /*
@@ -151,7 +151,7 @@ return [
         'reorder' => [
             // Define the size/looks of the content div for all CRUDs
             // To override per Controller use $this->crud->setReorderContentClass('class-string')
-            'contentClass'   => 'col-md-8 col-md-offset-2',
+            'contentClass'   => 'col-md-12 col-md-offset-2',
         ],
 
     ],
